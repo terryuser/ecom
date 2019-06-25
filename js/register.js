7a4c00a13f3a3b7aeed5a08987d20471349cd148
terryuser <homan03@hotmail.com>
2019-06-26 02:05:43 +0800

    26/6 02:05

--- js/register.js
+++ js/register.js
@@ -1,27 +1,54 @@
-$(document).ready(function () {
+$(document).ready(function() {
     register();
 });
 
 
 function register() {
-    $("#register-submit").click(function(){
-        var login = $("#reg-name").val();
-        var pass = $("#reg-password").val();
+    $("#register-submit").click(function() {
+        var name = $("#reg-name").val();
+        var password = $("#reg-password").val();
         var email = $("#reg-email").val();
 
-        var loginData = login + "&" + pass + "&" + email;
-        
-        $.ajax({
-            type: 'POST',
-            url: 'http://127.0.0.1:9000/api/sign-up',
-            dataType:"text",
-            data:loginData,
-            success: function(data) {
-                console.log("Sending register data: " + data);
-            },
-            error: function(xhr, status, error) {
-                console.log('Error: ' + error.message);
+        console.log("input name value: " + name);
+        console.log("input password value: " + password);
+        console.log("input email value: " + email);
+
+        checkInputNull("#reg-name", "Please provide your name.");
+        checkInputNull("#reg-password-confirm", "Please confirm your password.");
+        checkInputNull("#reg-password", "Please set up your password.");
+        checkInputNull("#reg-email", "Please provide your email.");
+
+        if ($("#reg-password-confirm").val() != password) {
+            $("#reg-password-confirm").parent().append("<div class='alertMsg'><p></p></div>");
+            console.log("confirm pw error");
+        } else {
+            if (!$("#agreement").prop("checked")) {
+                $("#agreement").parent().append("<div class='alertMsg'><p>Please agree of the policy.</p></div>");
+                console.log("confirm agreement error");
+            } else {
+                console.log("All confirm");
+                var loginData = name + "&" + password + "&" + email;
+                console.log("Sending to mongodb");
+                $.ajax({
+                    type: 'POST',
+                    url: 'http://127.0.0.1:9000/api/signup',
+                    dataType: "text",
+                    data: loginData,
+                    success: function(data) {
+                        console.log("Sending register data: " + data);
+                    },
+                    error: function(xhr, status, error) {
+                        console.log('Error: ' + error.message);
+                    }
+                });
             }
-        });
+        }
     });
+}
+
+function checkInputNull(target, msg) {
+    if ($(target).val() == null || $(target).val() == "") {
+        $(target).parent(".input-wrapper").append("<div class='alertMsg'><p>" + msg + "</p></div>");
+        return false;
+    }
 }
--- server.js
+++ server.js
@@ -1,168 +1,156 @@
 var http = require('http');
 var fs = require("fs");
 var qs = require('querystring');
-var MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 //var url = "mongodb+srv://ecom_admin:admin@ecom-member-vcel9.mongodb.net/test?retryWrites=true&w=majority";
 var dbUrl = "mongodb+srv://ecom_admin:admin@ecom-member-vcel9.mongodb.net/test?retryWrites=true&w=majority";
 var isLogin = false;
 
+
+
 http.createServer(function(request, response) {
 
-	if(request.url === "/"){
-		console.log("Request" + request.url + "success");
-		sendFileContent(response, "index.html", "text/html");
-	} 
-	else if (request.url === "/register") {
-		console.log("Request" + request.url + "success");
-		sendFileContent(response, "register.html", "text/html");
-	}
-	else if(request.url==="/api/sign-up"){
-		if (request.method === "POST") {
-			console.log("Registing account");
-			formData = '';
-			msg = '';
-			return request.on('data', function (data) {
+    if (request.url === "/") {
+        console.log("Request index page success");
+        sendFileContent(response, "index.html", "text/html");
+    } else if (request.url === "/register") {
+        console.log("Request" + request.url + " success");
+        sendFileContent(response, "register.html", "text/html");
+    } else if (request.url === "/api/signup") {
+        if (request.method === "POST") {
+            console.log("Registing account");
+            formData = '';
+            msg = '';
+            return request.on('data', function(data) {
 
-				//Get the register info
-				formData += data;
-				regit = formData.split("&");
+                //Get the register info
+                formData += data;
+                regit = formData.split("&");
 
-				return request.on('end', function () {
-					var user;
-					user = qs.parse(formData);
-					msg = JSON.stringify(user);
+                return request.on('end', function() {
+                    var user;
+                    user = qs.parse(formData);
+                    msg = JSON.stringify(user);
 
-					MongoClient.connect(dbUrl, function(err, db) {
+                    MongoClient.connect(dbUrl, function(err, db) {
 
-						if (err) throw err;
+                        if (err) throw err;
 
-						var dbo = db.db("member");
+                        var dbo = db.db("ecom");
 
-						var myobj = { login: regit[0] , password: regit[1] };
+                        var myobj = { login: regit[0], password: regit[1], email: regit[2] };
 
-						dbo.collection("customers").find(myobj).toArray(function(err, result) {
-							if (err) {
-								dbo.collection("customers").insertOne(myobj, function(err, res) {
+                        // dbo.collection("member").find(myobj).toArray(function(err, result) {
+                        //     if (err) {
+                        dbo.collection("member").insertOne(myobj, function(err, res) {
 
-									if (err) {
-										console.log("Acc already existed");
-									} else {
-										console.log("1 member regist success");
-										isLogin = true;
-									}
-		
-								});
-							} else {
-								console.log("login name duplicate");
-								response.end("Reg Failed");
-							}
+                            if (err) {
+                                console.log("Acc already existed");
+                            } else {
+                                console.log("1 member regist success");
+                                isLogin = true;
+                            }
 
-							db.close();
-						});
+                        });
+                        // } else {
+                        //     console.log("login name duplicate");
+                        //     response.end("Reg Failed");
+                        // }
 
-					});
-				});
-			});
-		} else {
-			//form = publicPath + "ajaxSignupForm.html";
-			sendFileContent(response, "index.html", "text/html");
-		}
-		 
-	}
-	else if(request.url==="/api/sign-in"){
-		if (request.method === "POST") {
-			console.log("Login action");
-			formData = '';
-			msg = '';
-			return request.on('data', function (data) {
-				formData += data;
+                        db.close();
+                        // });
 
-				info = formData.split("&");
-				console.log("name : " + info[0] + "pw : " + info[1]);
+                    });
+                });
+            });
+        } else {
+            //form = publicPath + "ajaxSignupForm.html";
+            sendFileContent(response, "index.html", "text/html");
+        }
 
-				return request.on('end', function () {
-					var user;
-					user = qs.parse(formData);
-					msg = JSON.stringify(user);
+    } else if (request.url === "/api/signin") {
+        if (request.method === "POST") {
+            console.log("Login action");
+            formData = '';
+            msg = '';
+            return request.on('data', function(data) {
+                formData += data;
 
-					MongoClient.connect(dburl, function(err, db) {
-						if (err) throw err;
-						var dbo = db.db("member");
-						var query = { login: info[0] , password: info[1]};
-						dbo.collection("customers").find(query).toArray(function(err, result) {
+                info = formData.split("&");
+                console.log("name : " + info[0] + "pw : " + info[1]);
 
-							if (!err) {
-								response.end("success");
-								console.log("login success, account: " + info[0]);
-								isLogin = true;
+                return request.on('end', function() {
+                    var user;
+                    user = qs.parse(formData);
+                    msg = JSON.stringify(user);
 
-								var array = [];
+                    MongoClient.connect(dburl, function(err, db) {
+                        if (err) throw err;
+                        var dbo = db.db("member");
+                        var query = { login: info[0], password: info[1] };
+                        dbo.collection("member").find(query).toArray(function(err, result) {
 
-								for (i = 0; i < result.length; i++) {
-									array.push(result[i].login);
-								}
+                            if (!err) {
+                                response.end("success");
+                                console.log("login success, account: " + info[0]);
+                                isLogin = true;
 
-								console.log(array.toString());
+                                var array = [];
 
-							} else {
-								response.end("failed");
-								isLogin = false;
+                                for (i = 0; i < result.length; i++) {
+                                    array.push(result[i].login);
+                                }
 
-								throw err;
-							}
+                                console.log(array.toString());
 
-							console.log(isLogin);
-							db.close();
-							
-						});
-					});
-				});
-			});
-		} else {
-			sendFileContent(response, "index.html", "text/html");
-		}
-	}
-	/*
-	else if(/^\/[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]*.js$/.test(request.url.toString())){
-		sendFileContent(response, request.url.toString().substring(1), "text/javascript");
-	}
-	else if(/^\/[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]*.css$/.test(request.url.toString())){
-		sendFileContent(response, request.url.toString().substring(1), "text/css");
-	}
-	else if(/^\/[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]*.jpg$/.test(request.url.toString())){
-		sendFileContent(response, request.url.toString().substring(1), "text/jpg");
-	}
-	else if(/^\/[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]*.png$/.test(request.url.toString())){
-		sendFileContent(response, request.url.toString().substring(1), "text/png");
-	}
-	else if(/^\/[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]*.min.js$/.test(request.url.toString())){
-		sendFileContent(response, request.url.toString().substring(1), "text/javascript");
-	}
-	else if(/^\/[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]*.min.css$/.test(request.url.toString())){
-		sendFileContent(response, request.url.toString().substring(1), "text/css");
-	}
-	else if(/^\/[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]*.tff$/.test(request.url.toString())){
-		sendFileContent(response, request.url.toString().substring(1), "text/css");
-	}*/
-	else if(/^\/[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]*.*$/.test(request.url.toString())){
-		sendFileContent(response, request.url.toString().substring(1), "text/css");
-	}
-	else{
-		console.log("Requested URL is: " + request.url);
-		response.end();
-	}
+                            } else {
+                                response.end("failed");
+                                isLogin = false;
+
+                                throw err;
+                            }
+
+                            console.log(isLogin);
+                            db.close();
+
+                        });
+                    });
+                });
+            });
+        } else {
+            sendFileContent(response, "index.html", "text/html");
+        }
+    } else if (/^\/[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]*.js$/.test(request.url.toString())) {
+        sendFileContent(response, request.url.toString().substring(1), "text/javascript");
+    } else if (/^\/[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]*.css$/.test(request.url.toString())) {
+        sendFileContent(response, request.url.toString().substring(1), "text/css");
+    } else if (/^\/[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]*.jpg$/.test(request.url.toString())) {
+        sendFileContent(response, request.url.toString().substring(1), "text/jpg");
+    } else if (/^\/[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]*.png$/.test(request.url.toString())) {
+        sendFileContent(response, request.url.toString().substring(1), "text/png");
+    } else if (/^\/[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]*.min.js$/.test(request.url.toString())) {
+        sendFileContent(response, request.url.toString().substring(1), "text/javascript");
+    } else if (/^\/[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]*.min.css$/.test(request.url.toString())) {
+        sendFileContent(response, request.url.toString().substring(1), "text/css");
+    } else if (/^\/[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]*.tff$/.test(request.url.toString())) {
+        sendFileContent(response, request.url.toString().substring(1), "text/css");
+    } else if (/^\/[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]*.*$/.test(request.url.toString())) {
+        sendFileContent(response, request.url.toString().substring(1), "text/css");
+    } else {
+        console.log("Requested URL is: " + request.url);
+        response.end();
+    }
 }).listen(9000)
 
-function sendFileContent(response, fileName, contentType){
-	fs.readFile(fileName, function(err, data){
-		if(err){
-			response.writeHead(404);
-			response.write("Not Found!");
-		}
-		else{
-			response.writeHead(200, {'Content-Type': contentType});
-			response.write(data);
-		}
-		response.end();
-	});
+function sendFileContent(response, fileName, contentType) {
+    fs.readFile(fileName, function(err, data) {
+        if (err) {
+            response.writeHead(404);
+            response.write("Not Found!");
+        } else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.write(data);
+        }
+        response.end();
+    });
 }
