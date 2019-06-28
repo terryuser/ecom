b50e9e9247c6e7408d1a7d70d313faac4f4fce15
terrylau1234 <terrylau@targa.com>
2019-06-28 11:32:26 +0800

    28/6 Fix post problem

--- routers/api.js
+++ routers/api.js
@@ -1,34 +1,17 @@
-//Import Library
 const express = require('express');
+const router = express.Router();
 const Member = require('../models/member');
 
-//Set up express
-const router = express.Router();
-
-
-//create member to db
-router.get('/reg', function(req, res) {
-    console.log('Server got data for sign up member');
-    res.send({ type: 'GET' });
+//Add member
+router.post('/register/submit', function(req,res){
+    Member.create(req.body).then(function(member){
+        res.send(member);
+    });
 });
 
-//create member to db
-router.post('/signupsubmit', function(req, res) {
-    // Member.create(req.body).then(function(member) {
-    //     res.send(member);
-    // });
-    console.log(req.body);
-    res.send({ type: 'POST' });
-});
-
-//update member to db
-router.put('/update/:id', function(req, res) {
-    res.send({ type: 'PUT' });
-});
-
-//delete data from db
-router.delete('/delete/:id', function(req, res) {
-    res.send({ type: 'DELETE' });
+//Get member information
+router.get('/member/:id', function(req,res){
+    res.send({type: 'GET member'});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,51 +1,32 @@
 //Import Library
 const express = require('express');
 const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
-const memberapi = require('./routers/api');
+const mongoose = require('mongoose');
 
 //Set up express
 const app = express();
-const router = express.Router();
+const path = require('path');
 
-// //Connet to mongodb
-// mongoose.connect('mongodb+srv://ecom_admin:admin@ecom-member-vcel9.mongodb.net/test?retryWrites=true&w=majority');
-// mongoose.Promise = global.Promise;
+//Connet to mongodb
+mongoose.connect('mongodb+srv://ecom_admin:admin@ecom-member-vcel9.mongodb.net/ecom', { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
 
-
-app.use(express.json());
-app.use(function(error, req, res, next) {
-    //Catch json error
-    sendError(res, myCustomErrorMessage);
-});
-
-
-//initalize routes
-app.use('/api', memberapi);
-
-
+//Define value
 const PORT = process.env.PORT = 3000;
 
-console.log(__dirname);
-
-//API location - href to html pages
+//Get html files
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
-    //__dirname : It will resolve to your project folder.
+    //__dirname : resolve to project folder.
 });
 
-app.get('/signup', function(req, res) {
-    res.sendFile(path.join(__dirname + '/signup.html'));
-    //__dirname : It will resolve to your project folder.
-});
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+app.use(bodyParser.json());
 
-app.get('/stock/:stockid', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-    //__dirname : It will resolve to your project folder.
-});
-
-
-
+//Allow routers
+app.use('/api', require('./routers/api'));
 
 //Allow fily type access
 app.use('/', express.static(__dirname + '/'));
@@ -53,8 +34,7 @@ app.use('/js', express.static(__dirname + '/js'));
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/img', express.static(__dirname + '/img'));
 
-//Listen for requests
-app.use('/', router);
+//Listen to request
 app.listen(PORT, () => {
     console.log('listening for requests')
 })
