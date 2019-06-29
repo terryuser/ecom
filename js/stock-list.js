0e7431a3e947b61fc8521e7a0322b952e900bd62
terryuser <homan03@hotmail.com>
2019-06-30 04:34:10 +0800

    30/6

--- js/stock-list.js
+++ js/stock-list.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
     stockSammary();
 });
 
@@ -11,40 +11,42 @@ function AddToFav() {
 }
 
 function stockSammary() {
+
+    var stockSymbol = {};
+
+    var apiKey = "7UQB9ISU3Q3RNEKI";
+
+    console.log(stockCode);
+
     var stockSammary = {
         "async": true,
         "crossDomain": true,
-        "url": "https://stock.p.rapidapi.com/v1/funds",
-        "method": "GET",
-        "headers": {
-            "X-RapidAPI-Host": "stock.p.rapidapi.com",
-            "X-RapidAPI-Key": "aa4a4ca3famsh848267a0a99e223p125cc3jsn3ec1cfd6d129"
-        }
+        "url": "http://hq.sinajs.cn/list=hk00001",
+        "method": "GET"
     }
 
-    $.ajax(stockSammary).done(function (response) {
-        var data = response.result.funds;
-        $("#updateStamp").append('Updated on' + response.result.timestamp);
+    $.ajax(stockSammary).done(function(response) {
+        var data = response.split('"');
 
-        $.each(data, function (i, item) {
 
-            //Define stock item format
-            var keySymbol = '<div class"key symbol">'+ data[i].symbol + '</div>';
-            var keyName = '<div class="key name">'+ data[i].name + '</div>';
-            var keyExchange = '<div class="key exchange">'+ data[i].exchange + '</div>';
-            var keyChange = '<div class="key change">'+ data[i].change + '</div>';
-            var keyStrength = '<div class="key rsi">'+ data[i].strength + '</div>';
-            var keyPassion = '<div class="key passion">'+ data[i].passion + '</div>';
-            var keyReach = '<div class="key reach">'+ data[i].reach + '</div>';
+        $("#updateStamp").append('Updated on' + data);
 
-            var addFavBTN = '<button class="addFav" data="' + data[i].symbol + '">add</button>';
+        // //Define stock item format
+        // var keySymbol = '<div class"key code">' + stockCode + '</div>';
+        // var keyName = '<div class="key name">' + data[i].name + '</div>';
+        // var keyExchange = '<div class="key exchange">' + data[i].exchange + '</div>';
+        // var keyChange = '<div class="key change">' + data[i].change + '</div>';
+        // var keyStrength = '<div class="key rsi">' + data[i].strength + '</div>';
+        // var keyPassion = '<div class="key passion">' + data[i].passion + '</div>';
+        // var keyReach = '<div class="key reach">' + data[i].reach + '</div>';
 
-            var listItemHTML = keySymbol + keyName + keyExchange + keyChange + keyStrength + keyPassion + keyReach;
+        // var addFavBTN = '<button class="addFav" data="' + data[i].symbol + '">add</button>';
 
-            $("#stock-list-block").append('<div class="listItem" id=' + data[i].symbol + '>' + listItemHTML + '</div>' + addFavBTN);
-        });
+        // var listItemHTML = keySymbol + keyName + keyExchange + keyChange + keyStrength + keyPassion + keyReach;
+
+        // $("#stock-list-block").append('<div class="listItem" id=' + data[i].symbol + '>' + listItemHTML + '</div>' + addFavBTN);
+        $("#stock-list-block").append('<div class="listItem">' + listItemHTML + '</div>');
 
         AddToFav();
     });
 }
-
